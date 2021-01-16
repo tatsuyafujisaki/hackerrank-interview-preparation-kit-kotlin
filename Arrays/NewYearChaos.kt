ba05177c7c1be22647a0fdfc7a2c5cee66e9e94f
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2021-01-17 00:13:04 +0900

    Create NewYearChaos.kt

---
+++ Arrays/NewYearChaos.kt
@@ -0,0 +1,36 @@
+fun minimumBribes(q: List<Int>) {
+    var expected1 = 1
+    var expected2 = 2
+    var expected3 = 3
+    var bribes = 0
+    for (i in q.indices) {
+        when(q[i]) {
+            expected1 -> {
+                expected1 = expected2
+                expected2 = expected3
+                expected3++
+            }
+            expected2 -> {
+                expected2 = expected3
+                expected3++
+                bribes++
+            }
+            expected3 -> {
+                expected3++
+                bribes += 2
+            }
+            else -> {
+                println("Too chaotic")
+                return
+            }
+        }
+    }
+    println(bribes)
+}
+
+fun main() {
+    repeat(readLine().orEmpty().toInt()) {
+        readLine() // Read and discard
+        minimumBribes(readLine().orEmpty().split(' ').map { it.toInt() })
+    }
+}
