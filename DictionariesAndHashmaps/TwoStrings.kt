8b18ca13b6b62cafadcf15df43a1e892e6103423
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2021-01-18 21:09:37 +0900

    Create TwoStrings.kt

---
+++ DictionariesAndHashmaps/TwoStrings.kt
@@ -0,0 +1,14 @@
+fun twoStrings(s1: String, s2: String) = if (s1.toSet().intersect(s2.toSet()).isEmpty()) "NO" else "YES"
+
+fun main() {
+    val t = readLine().orEmpty().toInt()
+    val ss = mutableListOf<Pair<String, String>>()
+    repeat(t) {
+        val s1 = readLine().orEmpty()
+        val s2 = readLine().orEmpty()
+        ss.add(s1 to s2)
+    }
+    for(s in ss) {
+        println(twoStrings(s.first, s.second))
+    }
+}
