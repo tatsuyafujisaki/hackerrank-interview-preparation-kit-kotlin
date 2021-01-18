102a49bfe9b661f3b51f6841aa4a25f7b42f3993
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2021-01-18 21:13:15 +0900

    Update TwoStrings.kt

--- DictionariesAndHashmaps/TwoStrings.kt
+++ DictionariesAndHashmaps/TwoStrings.kt
@@ -1,4 +1,4 @@
-fun twoStrings(s1: String, s2: String) = if (s1.toSet().intersect(s2.toSet()).isEmpty()) "NO" else "YES"
+fun twoStrings(s1: String, s2: String) = if (s1.toSet().intersect(s2.toSet()).isNotEmpty()) "YES" else "NO"
 
 fun main() {
     val t = readLine().orEmpty().toInt()
