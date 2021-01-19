081e13f8f38a0b7be1b802147c8621146ca4334c
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2021-01-19 22:26:25 +0900

    Update SherlockAndAnagrams.kt

--- DictionariesAndHashmaps/SherlockAndAnagrams.kt
+++ DictionariesAndHashmaps/SherlockAndAnagrams.kt
@@ -1,29 +1,21 @@
-fun String.alphabetized() = toCharArray().sorted().joinToString("")
-
-fun MutableMap<String, Int>.increment(key: String) = merge(key, 1, Int::plus)
-
 fun sherlockAndAnagrams(s: String): Int {
-    var anagramCount = 0
+    var count = 0
     val anagrams = mutableMapOf<String, Int>()
-
     for (i in s.indices) {
         for (j in i + 1..s.length) {
-            val substring = s.substring(i, j).alphabetized()
-            anagramCount += anagrams.getOrPut(substring) { 0 }
-            anagrams.increment(substring)
+            val substring = s.substring(i, j).toCharArray().sorted().joinToString("")
+            count += anagrams.getOrPut(substring) { 0 }
+            anagrams.merge(substring, 1, Int::plus)
         }
     }
-
-    return anagramCount
+    return count
 }
 
 fun main() {
     val strings = mutableListOf<String>()
-
     repeat(readLine().orEmpty().toInt()) {
         strings.add(readLine().orEmpty())
     }
-
     for (string in strings) {
         println(sherlockAndAnagrams(string))
     }
