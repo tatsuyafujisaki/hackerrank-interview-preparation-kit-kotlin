03d6d5aa6c7e7525e8a92271206ee45e88515c9b
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2021-01-23 01:02:24 +0900

    Update ArraysLeftRotation.kt

--- Arrays/ArraysLeftRotation.kt
+++ Arrays/ArraysLeftRotation.kt
@@ -1,7 +1,7 @@
 fun rotLeft(a: Collection<Int>, d: Int) = a.drop(d) + a.take(d)
 
 fun main() {
-    val d = readLine().orEmpty().split(' ').map { it.toInt() }[1]
-    val a = readLine().orEmpty().split(' ').map { it.toInt() }
+    val d = readLine().orEmpty().split(' ').map(String::toInt)[1]
+    val a = readLine().orEmpty().split(' ').map(String::toInt)
     rotLeft(a, d).forEach { print("$it ") }
 }
