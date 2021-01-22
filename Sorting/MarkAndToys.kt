05f672ad168f973850f12c30c68bc10f694d1625
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2021-01-23 01:56:45 +0900

    Create MarkAndToys.kt

---
+++ Sorting/MarkAndToys.kt
@@ -0,0 +1,18 @@
+import kotlin.String
+
+fun maximumToys(prices: List<Int>, k: Int): Int {
+    var totalPrice = 0
+    prices.sorted().forEachIndexed { i, price ->
+        totalPrice += price
+        if (totalPrice > k) {
+            return i
+        }
+    }
+    return prices.size
+}
+
+fun main() {
+    val k = readLine().orEmpty().split(' ').map(String::toInt)[1]
+    val prices = readLine().orEmpty().split(' ').map(String::toInt)
+    println(maximumToys(prices, k))
+}
