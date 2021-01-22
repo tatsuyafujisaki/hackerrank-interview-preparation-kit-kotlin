e92c6c1dd126bec1841d8ca84f503e5bb25d76f3
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2021-01-22 22:19:18 +0900

    Update CountTriplets.kt

--- DictionariesAndHashmaps/CountTriplets.kt
+++ DictionariesAndHashmaps/CountTriplets.kt
@@ -1,14 +1,14 @@
-fun <T> MutableMap<T, Long>.increment(key: T, value: Long = 1) {
+fun MutableMap<Long, Long>.increment(key: Long, value: Long = 1) {
     merge(key, value, Long::plus)
 }
 
 fun countTriplets(arr: List<Long>, r: Long): Long {
-    val singlets = mutableMapOf<Long, Long>()
-    val doublets = mutableMapOf<Long, Long>()
+    val singlets = mutableMapOf<Long, Long>().withDefault { 0 }
+    val doublets = mutableMapOf<Long, Long>().withDefault { 0 }
     var triplets = 0L
     for (i in arr.indices) {
-        triplets += doublets.getOrDefault(arr[i], 0)
-        doublets.increment(arr[i] * r, singlets.getOrDefault(arr[i], 0))
+        triplets += doublets.getValue(arr[i])
+        doublets.increment(arr[i] * r, singlets.getValue(arr[i]))
         singlets.increment(arr[i] * r)
     }
     return triplets
