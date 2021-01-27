28c3ee1d8ca5624f6ae841a88aad4bc98091e9cf
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2021-01-27 22:42:40 +0900

    Update SherlockAndAnagrams.kt

--- DictionariesAndHashmaps/SherlockAndAnagrams.kt
+++ DictionariesAndHashmaps/SherlockAndAnagrams.kt
@@ -9,11 +9,7 @@ fun String.substrings() = sequence {
 fun sherlockAndAnagrams(s: String) = s.substrings().map { it.toCharArray().sorted() }.groupingBy { it }.eachCount().map { it.value }.sumBy { (it * (it - 1)) / 2 }
 
 fun main() {
-    val strings = mutableListOf<String>()
     repeat(readLine().orEmpty().toInt()) {
-        strings.add(readLine().orEmpty())
-    }
-    for (string in strings) {
-        println(sherlockAndAnagrams(string))
+        println(sherlockAndAnagrams(readLine().orEmpty()))
     }
 }
