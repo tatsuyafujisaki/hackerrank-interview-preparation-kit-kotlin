2c32c239bf308efbdaba764e5b89d65d94670f1a
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2021-01-29 14:59:47 +0900

    Update AlternatingCharacters.kt

--- StringManipulation/AlternatingCharacters.kt
+++ StringManipulation/AlternatingCharacters.kt
@@ -1,8 +1,6 @@
 fun alternatingCharacters(s: String): Int {
     var count = 0
-    for (i in 0 until s.length - 1) {
-        if (s[i] == s[i + 1]) count++
-    }
+    for (i in 0 until s.lastIndex) if (s[i] == s[i + 1]) count++
     return count
 }
 
