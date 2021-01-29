02dfb0be30c0dd1fc66c8c596878e88d7587ea03
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2021-01-29 15:04:25 +0900

    Update JumpingOnTheClouds.kt

--- WarmUpChallenges/JumpingOnTheClouds.kt
+++ WarmUpChallenges/JumpingOnTheClouds.kt
@@ -1,7 +1,7 @@
 fun jumpingOnClouds(clouds: List<Int>): Int {
     var jumps = 0
     var i = 0
-    while (i < clouds.size - 1) {
+    while (i < clouds.lastIndex) {
         i += if (i + 2 < clouds.size && clouds[i + 2] == 0) 2 else 1
         jumps++
     }
