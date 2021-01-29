9dbb6780bfa824c3c8ba424d7d6288fd47a85df1
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2021-01-29 10:40:15 +0900

    Create SpecialStringAgain.kt

---
+++ StringManipulation/SpecialStringAgain.kt
@@ -0,0 +1,33 @@
+import kotlin.math.min
+
+// https://www.geeksforgeeks.org/count-special-palindromes-in-a-string
+fun substrCount(s: String): Int {
+    val counts = sequence {
+        var count = 0
+        var previous = 0.toChar()
+        for (c in s) {
+            if (previous == c) {
+                count++
+            } else {
+                yield(previous to count)
+                previous = c
+                count = 1
+            }
+        }
+        yield(previous to count)
+    }.toList()
+    // Sum of n! -> n(a_1+a_n)/2 -> n(n+1)/2
+    // e.g. "aaa" -> "aaa", "aa", "aa", "a", "a", "a" -> 3(3+1)/2
+    var total = counts.map { it.second }.sumBy { it * (it + 1) / 2 }
+    for (i in 1 until counts.size - 1) {
+        if (counts[i].second == 1 && counts[i - 1].first == counts[i + 1].first) {
+            total += min(counts[i - 1].second, counts[i + 1].second)
+        }
+    }
+    return total
+}
+
+fun main() {
+    readLine() // Read and discard
+    println(substrCount(readLine().orEmpty()))
+}
