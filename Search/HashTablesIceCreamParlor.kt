e76372d65f59b354cafb9649646a7659b353619a
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2021-01-31 15:16:18 +0900

    Create HashTablesIceCreamParlor.kt

---
+++ Search/HashTablesIceCreamParlor.kt
@@ -0,0 +1,19 @@
+fun whatFlavors(cost: List<Int>, money: Int) {
+    val flavors = mutableMapOf<Int, Int>()
+    cost.forEachIndexed { i, c ->
+        flavors[money - c]?.let {
+            println("$it ${i + 1}")
+            return
+        }
+        flavors[c] = i + 1
+    }
+}
+
+fun main() {
+    repeat(readLine().orEmpty().toInt()) {
+        val money = readLine().orEmpty().toInt()
+        readLine() // Read and discard
+        // Costs in all the non-sample test cases contain a trailing space, which will causes a parsing error.
+        whatFlavors(readLine().orEmpty().split(' ').filterNot(String::isEmpty).map(String::toInt), money)
+    }
+}
