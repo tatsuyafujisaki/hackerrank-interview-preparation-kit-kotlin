6ecf5de1a64122f2efbcda62861be782803e0de8
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2021-02-15 21:18:25 +0900

    Update BalancedBrackets.kt

--- Stacks/BalancedBrackets.kt
+++ Stacks/BalancedBrackets.kt
@@ -1,12 +1,14 @@
 import java.util.Stack
 
+fun Stack<*>.popOrNull() = if (empty()) null else pop()
+
 fun isBalanced(s: String) = with(Stack<Char>()) {
     for (c in s) {
         when (c) {
             '{' -> push('}')
             '[' -> push(']')
             '(' -> push(')')
-            else -> if (isEmpty() || c != pop()) return "NO"
+            else -> if (c != popOrNull()) return "NO"
         }
     }
     if (empty()) "YES" else "NO"
