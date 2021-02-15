03251c0848676275a885096d97e4b66ad9b23ea0
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2021-02-15 12:33:14 +0900

    Update DecibinaryNumbers.kt

--- DynamicProgramming/DecibinaryNumbers.kt
+++ DynamicProgramming/DecibinaryNumbers.kt
@@ -37,16 +37,17 @@ fun decibinaryNumbers(f: List<LongArray>, c: LongArray, x: Long): Long {
     var decibinaryDigits = ""
     for (d in maxD downTo 1) {
         var i = 0
-        var previousDecibinaryCount = 0L
         var decibinaryCount = 0L
+        var temp = 0L
         while (decibinaryCount < g) {
-            previousDecibinaryCount = decibinaryCount
+            temp = decibinaryCount
             decibinaryCount += f[d - 1][s - i++ * pow2[d - 1]]
         }
         i--
+        decibinaryCount = temp
         decibinaryDigits += i
         s -= i * pow2[d - 1]
-        g -= previousDecibinaryCount
+        g -= decibinaryCount
     }
     return decibinaryDigits.toLong()
 }
