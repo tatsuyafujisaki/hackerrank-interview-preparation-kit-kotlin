7d1fa33e3146fee50c33592a314c3cfb7b11e8e7
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2021-03-31 19:27:51 +0900

    Update MinMaxRiddle.kt

--- StacksAndQueues/MinMaxRiddle.kt
+++ StacksAndQueues/MinMaxRiddle.kt
@@ -1,8 +1,6 @@
 import java.util.Stack
 import kotlin.math.max
 
-fun <T> Stack<T>.peekOrElse(item: T) = lastOrNull() ?: item
-
 /** Remove this function and use the built-in removeLast() after Kotlin 1.4 */
 fun MutableList<*>.removeLast() {
     removeAt(lastIndex)
@@ -13,7 +11,7 @@ fun findSmallerLeft(xs: List<Int>): IntArray {
     return IntArray(xs.size).apply {
         for (i in xs.indices) {
             while (stack.isNotEmpty() && xs[stack.peek()] >= xs[i]) stack.removeLast()
-            this[i] = stack.peekOrElse(-1)
+            this[i] = stack.lastOrNull() ?: -1
             stack.push(i)
         }
     }
@@ -24,22 +22,22 @@ fun findSmallerRight(xs: List<Int>): IntArray {
     return IntArray(xs.size).apply {
         for (i in xs.lastIndex downTo 0) {
             while (stack.isNotEmpty() && xs[stack.peek()] >= xs[i]) stack.removeLast()
-            this[i] = stack.peekOrElse(xs.size)
+            this[i] = stack.lastOrNull() ?: xs.size
             stack.push(i)
         }
     }
 }
 
+// https://www.geeksforgeeks.org/find-the-maximum-of-minimums-for-every-window-size-in-a-given-array/
 fun riddle(arr: List<Int>): List<Int> {
     val left = findSmallerLeft(arr)
     val right = findSmallerRight(arr)
-    return with(IntArray(arr.size + 1)) {
+    return MutableList(arr.size) { 0 }.apply {
         for (i in arr.indices) {
             val windowSize = right[i] - left[i] - 1
-            this[windowSize] = max(this[windowSize], arr[i])
+            this[windowSize - 1] = max(this[windowSize - 1], arr[i])
         }
-        for (i in arr.lastIndex downTo 1) this[i] = max(this[i], this[i + 1])
-        drop(1)
+        for (i in lastIndex - 1 downTo 0) this[i] = max(this[i], this[i + 1])
     }
 }
 
