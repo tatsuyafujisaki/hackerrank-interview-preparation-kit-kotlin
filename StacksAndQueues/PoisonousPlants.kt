b1f07a076e3f1762958b159c5560e16e7cecde8d
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2021-04-02 00:03:45 +0900

    Update PoisonousPlants.kt

--- StacksAndQueues/PoisonousPlants.kt
+++ StacksAndQueues/PoisonousPlants.kt
@@ -1,22 +1,22 @@
 fun poisonousPlants(p: List<Int>): Int {
-    val lists = mutableListOf(mutableListOf(p.first())).apply {
+    val listOfDescendingPlantLists = mutableListOf(mutableListOf(p.first())).apply {
         for (i in 1 until p.size) {
-            val list = last()
-            if (p[i] <= list.last()) list.add(p[i]) else add(mutableListOf(p[i]))
+            val descendingPlantList = last()
+            if (p[i] <= descendingPlantList.last()) descendingPlantList.add(p[i]) else add(mutableListOf(p[i]))
         }
     }
     var days = 0
-    while (lists.size > 1) {
-        for (i in 1 until lists.size) lists[i].removeAt(0) // TODO: Use removeFirst() in Kotlin 1.4+.
-        lists.removeIf {
+    while (listOfDescendingPlantLists.size > 1) {
+        for (i in 1 until listOfDescendingPlantLists.size) listOfDescendingPlantLists[i].removeAt(0) // TODO: Use removeFirst() in Kotlin 1.4+.
+        listOfDescendingPlantLists.removeIf {
             it.isEmpty()
         }
         do {
             var merged = false
-            for (i in 0 until lists.lastIndex) {
-                if (lists[i].last() >= lists[i + 1].first()) {
-                    lists[i].addAll(lists[i + 1])
-                    lists.removeAt(i + 1)
+            for (i in 0 until listOfDescendingPlantLists.lastIndex) {
+                if (listOfDescendingPlantLists[i].last() >= listOfDescendingPlantLists[i + 1].first()) {
+                    listOfDescendingPlantLists[i].addAll(listOfDescendingPlantLists[i + 1])
+                    listOfDescendingPlantLists.removeAt(i + 1)
                     merged = true
                     break
                 }
