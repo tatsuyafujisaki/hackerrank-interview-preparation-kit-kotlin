5fab638f1b2336c25f6c2c280bdbe15e5f6c0733
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2021-04-10 22:12:59 +0900

    Update FindTheNearestClone.java

--- Graphs/FindTheNearestClone.java
+++ Graphs/FindTheNearestClone.java
@@ -11,50 +11,37 @@ public class Solution {
     public static void main(String[] args) {
         FastScanner fs = new FastScanner();
         PrintWriter pw = new PrintWriter(System.out);
-
         int n = fs.nextInt();
         int m = fs.nextInt();
-
         ArrayList<ArrayList<Integer>> graph = new ArrayList<>(n);
-
         for (int i = 0; i < n; i++) graph.add(new ArrayList<>());
-
         for (int i = 0; i < m; i++) {
             int from = fs.nextZeroBasedInt();
             int to = fs.nextZeroBasedInt();
             graph.get(from).add(to);
             graph.get(to).add(from);
         }
-
         int[] colors = fs.nextZeroBasedIntegers(n);
         int targetColor = fs.nextZeroBasedInt();
-
         fs.close();
-
         ArrayList<Integer> origins = new ArrayList<>();
         for (int i = 0; i < n; i++) {
             if (colors[i] == targetColor) origins.add(i);
         }
-
         if (origins.size() == 1) {
             pw.println(-1);
             pw.close();
             return;
         }
-
         int[] distancesFromNearestOrigins = new int[n];
         Arrays.fill(distancesFromNearestOrigins, -1);
-
         int[] originsForEachNode = new int[n];
         Arrays.fill(originsForEachNode, -1);
-
         for (int origin : origins) {
             distancesFromNearestOrigins[origin] = 0;
             originsForEachNode[origin] = origin;
         }
-
         ArrayDeque<Integer> nodesToVisit = new ArrayDeque<>(origins);
-
         while (!nodesToVisit.isEmpty()) {
             int currentNode = nodesToVisit.removeFirst();
             for (int adjacentNode : graph.get(currentNode)) {
@@ -69,7 +56,6 @@ public class Solution {
                 }
             }
         }
-
         pw.println(-1);
         pw.close();
     }
