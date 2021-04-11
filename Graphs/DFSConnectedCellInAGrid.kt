eca1f6f9a6e75744fb85c77b60aed0d957b223d1
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2021-04-11 21:26:53 +0900

    Update DFSConnectedCellInAGrid.kt

--- Graphs/DFSConnectedCellInAGrid.kt
+++ Graphs/DFSConnectedCellInAGrid.kt
@@ -1,3 +1,5 @@
+import kotlin.math.max
+
 fun maxRegion(grid: Array<BooleanArray>): Int {
     fun count(row: Int, column: Int): Int = if (row < 0 || column < 0 || row >= grid.size || column >= grid.first().size || !grid[row][column]) {
         0
@@ -8,21 +10,15 @@ fun maxRegion(grid: Array<BooleanArray>): Int {
             count(row + 1, column - 1) + count(row + 1, column) + count(row + 1, column + 1)
     }
 
-    return grid.indices.zip(grid.first().indices).map { count(it.first, it.second) }.max()!!
+    var result = 0
+    for (row in 1..grid.size - 2) {
+        for (column in 1..grid.first().size - 2) result = max(result, count(row, column))
+    }
+    return result
 }
 
 fun main() {
     val n = readLine().orEmpty().toInt()
     readLine() // Read and discard
-    println(
-        maxRegion(
-            Array(n) {
-                readLine()
-                    .orEmpty()
-                    .split(' ')
-                    .map { it == "1" }
-                    .toBooleanArray()
-            }
-        )
-    )
+    println(maxRegion(Array(n) { readLine().orEmpty().split(' ').map { it == "1" }.toBooleanArray() }))
 }
