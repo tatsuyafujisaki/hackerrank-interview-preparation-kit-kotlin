b5c8f533404384305a3d153d98c92168871ff563
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2021-04-12 00:07:36 +0900

    Create TreesIsThisABinarySearchTree.java

---
+++ Trees/TreesIsThisABinarySearchTree.java
@@ -0,0 +1,7 @@
+boolean checkBST(Node root) {
+    return checkBST(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
+}
+
+private boolean checkBST(Node node, int left, int right) {
+    return node == null || left < node.data && node.data < right && checkBST(node.left, left, node.data) && checkBST(node.right, node.data, right);
+}
