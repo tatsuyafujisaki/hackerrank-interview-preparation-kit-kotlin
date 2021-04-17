79a852dba7604f987deb98cd5b0c23166cea6560
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2021-04-17 15:11:52 +0900

    Update LuckBalance.kt

--- GreedyAlgorithms/LuckBalance.kt
+++ GreedyAlgorithms/LuckBalance.kt
@@ -1,4 +1,4 @@
-fun luckBalance(k: Int, contests: Array<IntArray>) = with(contests.partition { it[1] == 1 }) {
+fun luckBalance(k: Int, contests: Iterable<IntArray>) = with(contests.partition { it[1] == 1 }) {
     with(first.map { it.first() }.sortedDescending()) {
         take(k).sum() - drop(k).sum()
     } + second.map { it.first() }.sum()
@@ -6,7 +6,7 @@ fun luckBalance(k: Int, contests: Array<IntArray>) = with(contests.partition { i
 
 fun main() {
     val (n, k) = readLine().orEmpty().split(' ').map(String::toInt)
-    val contests = Array(n) {
+    val contests = List(n) {
         readLine().orEmpty().split(' ').map(String::toInt).toIntArray()
     }
     println(luckBalance(k, contests))
