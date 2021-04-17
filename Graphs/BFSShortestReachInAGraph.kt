84a0619b8b47e4c8822b0e53bc5c6a1cf0bf11f9
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2021-04-17 11:51:08 +0900

    Update BFSShortestReachInAGraph.kt

--- Graphs/BFSShortestReachInAGraph.kt
+++ Graphs/BFSShortestReachInAGraph.kt
@@ -2,15 +2,19 @@ fun main() {
     val edgeLength = 6
     repeat(readLine().orEmpty().toInt()) {
         val (n, m) = readLine().orEmpty().split(' ').map(String::toInt)
-        val graph = Array(n) { mutableSetOf<Int>() }
+        val graph = List(n) { mutableSetOf<Int>() }
         repeat(m) {
-            val (u, v) = readLine().orEmpty().split(' ').map(String::toInt).map { it - 1 } // to zero-based
+            val (u, v) = readLine()
+                .orEmpty()
+                .split(' ')
+                .map(String::toInt)
+                .map { it - 1 }  // converts to zero-based numbering.
             graph[u].add(v)
             graph[v].add(u)
         }
-        val s = readLine().orEmpty().toInt() - 1 // to zero-based
-        val distances = IntArray(n) {
-            if (it == s) 0 else -1
+        val s = readLine().orEmpty().toInt() - 1 // converts to zero-based numbering.
+        val distances = IntArray(n) { -1 }.apply {
+            this[s] = 0
         }
         val nodesToVisit = mutableListOf(s)
         while (nodesToVisit.isNotEmpty()) {
