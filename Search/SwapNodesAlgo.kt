2eb1ee72642b1c6bf17caf5bcef842def0ac7ef3
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2021-04-17 14:58:28 +0900

    Update SwapNodesAlgo.kt

--- Search/SwapNodesAlgo.kt
+++ Search/SwapNodesAlgo.kt
@@ -6,31 +6,36 @@ data class Node(var left: Int, var right: Int) {
     }
 }
 
-fun MutableList<Node>.swapSubtrees(k: Int, h: Int = 1, index: Int = 1) {
+fun swapSubtrees(nodes: List<Node>, k: Int, h: Int = 1, index: Int = 1) {
     if (index == -1) return
-    val node = this[index - 1]
+    val node = nodes[index - 1]
     if (h % k == 0) node.swapChildren()
-    swapSubtrees(k, h + 1, node.left)
-    swapSubtrees(k, h + 1, node.right)
+    swapSubtrees(nodes, k, h + 1, node.left)
+    swapSubtrees(nodes, k, h + 1, node.right)
 }
 
-fun List<Node>.traverseInOrder(index: Int = 1): List<Int> = if (index == -1) emptyList() else traverseInOrder(this[index - 1].left) + index + traverseInOrder(this[index - 1].right)
+fun traverseInOrder(nodes: List<Node>, index: Int = 1): List<Int> = if (index == -1) {
+    emptyList()
+} else traverseInOrder(nodes, nodes[index - 1].left) + index + traverseInOrder(
+    nodes,
+    nodes[index - 1].right
+)
 
-fun swapNodes(indexes: Array<IntArray>, queries: Array<Int>): List<List<Int>> {
+fun swapNodes(indices: Iterable<IntArray>, queries: Iterable<Int>): List<List<Int>> {
     val nodes = mutableListOf<Node>()
-    for (index in indexes) nodes.add(Node(index[0], index[1]))
+    for (index in indices) nodes.add(Node(index[0], index[1]))
     return queries.map {
-        nodes.swapSubtrees(it)
-        nodes.traverseInOrder()
+        swapSubtrees(nodes, it)
+        traverseInOrder(nodes)
     }
 }
 
 fun main() {
-    val indexes = Array(readLine().orEmpty().toInt()) {
+    val indices = List(readLine().orEmpty().toInt()) {
         readLine().orEmpty().split(' ').map(String::toInt).toIntArray()
     }
-    val queries = Array(readLine().orEmpty().toInt()) {
+    val queries = List(readLine().orEmpty().toInt()) {
         readLine().orEmpty().toInt()
     }
-    for (nodes in swapNodes(indexes, queries)) println(nodes.joinToString(" "))
+    for (nodes in swapNodes(indices, queries)) println(nodes.joinToString(" "))
 }
