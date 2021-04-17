df600dd8129bb762b046ee90708003eae5e78fb9
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2021-04-17 14:43:00 +0900

    Update CommonChild.kt

--- StringManipulation/CommonChild.kt
+++ StringManipulation/CommonChild.kt
@@ -1,6 +1,6 @@
 import kotlin.math.max
 
-fun commonChild(s1: String, s2: String) = Array(s1.length + 1) { IntArray(s2.length + 1) }.also {
+fun commonChild(s1: String, s2: String) = List(s1.length + 1) { IntArray(s2.length + 1) }.also {
     for (i in s1.indices) {
         for (j in s2.indices) it[i + 1][j + 1] = if (s1[i] == s2[j]) it[i][j] + 1 else max(it[i + 1][j], it[i][j + 1])
     }
