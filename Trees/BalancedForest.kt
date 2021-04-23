5e078a4928d50ee847472635e71328f22805a337
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2021-04-23 23:15:38 +0900

    Update BalancedForest.kt

--- Trees/BalancedForest.kt
+++ Trees/BalancedForest.kt
@@ -1,9 +1,10 @@
+import util.SimulatedStandardInput
+
 val Long.isOdd get() = this % 2 == 1L
-fun <K, V : Comparable<V>> Map<K, V>.sortedByValue() = toList().sortedBy { it.second }.toMap()
 
 data class Vertex(val ancestors: List<Int>, val children: List<Int>, val subtreeSum: Long)
 
-fun findRootOfMinHeightTree(graph: List<Set<Int>>): Set<Int> {
+fun findRootOfMinHeightTree(graph: List<List<Int>>): Set<Int> {
     // Early return if the graph has only one vertex because the rest of findRootOfMinHeightTree() mistakenly returns an empty set in the scenario.
     if (graph.size == 1) return setOf(0)
     var remainingVertices = graph.size
@@ -23,23 +24,26 @@ fun findRootOfMinHeightTree(graph: List<Set<Int>>): Set<Int> {
     return leaves.toSet()
 }
 
-fun convertGraphToTree(graph: List<Set<Int>>, data: List<Int>, root: Int): List<Vertex> {
+fun convertGraphToTree(graph: List<List<Int>>, data: List<Int>, root: Int): List<Vertex> {
     val ancestors = Array<List<Int>>(graph.size) { emptyList() }
     val vertices = Array<Vertex?>(graph.size) { null }
 
     fun visitVertex(id: Int, parent: Int) {
-        ancestors[id] = if (parent == -1) emptyList() else listOf(parent) + ancestors[parent]
+        ancestors[id] = listOf(parent) + ancestors[parent]
         val children = graph[id].filter { it != root && ancestors[it].isEmpty() }
         for (child in children) visitVertex(child, id)
         val subtreeData = data[id] + children.map { vertices[it]!!.subtreeSum }.sum()
-        vertices[id] = Vertex(ancestors[id], children.sortedBy { vertices[it]!!.subtreeSum }, subtreeData)
+        vertices[id] = Vertex(ancestors[id], children, subtreeData)
     }
 
-    visitVertex(root, -1)
+    for (child in graph[root]) visitVertex(child, root)
+    val subtreeData = data[root] + graph[root].map { vertices[it]!!.subtreeSum }.sum()
+    vertices[root] = Vertex(ancestors[root], graph[root], subtreeData)
+
     return vertices.filterNotNull()
 }
 
-fun balancedForest(graph: List<Set<Int>>, c: List<Int>): Long {
+fun balancedForest(graph: List<List<Int>>, c: List<Int>): Long {
     val root = findRootOfMinHeightTree(graph).first()
     val vertices = convertGraphToTree(graph, c, root)
     val sortedAncestors = vertices.map { it.ancestors.sorted() }
@@ -104,10 +108,12 @@ fun balancedForest(graph: List<Set<Int>>, c: List<Int>): Long {
 }
 
 fun main() {
+    val stdin = SimulatedStandardInput("input04.txt")
+    fun readLine() = stdin.readLine()
     repeat(readLine().orEmpty().toInt()) {
         val n = readLine().orEmpty().toInt()
         val c = readLine().orEmpty().split(' ').map(String::toInt)
-        val graph = List(n) { mutableSetOf<Int>() }
+        val graph = List(n) { mutableListOf<Int>() }
         repeat(n - 1) {
             val (v1, v2) = readLine()
                 .orEmpty()
