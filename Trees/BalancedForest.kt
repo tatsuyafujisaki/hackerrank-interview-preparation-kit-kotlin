4e57670935fa4c0e1d3dabf2baf94526ac45a68f
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2021-04-24 20:00:50 +0900

    Update BalancedForest.kt

--- Trees/BalancedForest.kt
+++ Trees/BalancedForest.kt
@@ -1,4 +1,6 @@
-val Long.isOdd get() = this % 2 == 1L
+import util.SimulatedStandardInput
+
+val Long.isEven get() = this % 2 == 0L
 
 data class Vertex(val ancestors: List<Int>, val children: List<Int>, val subtreeSum: Long)
 
@@ -28,7 +30,7 @@ fun convertGraphToTree(graph: List<List<Int>>, data: List<Int>, root: Int): List
 
     fun visitVertex(id: Int, parent: Int) {
         ancestors[id] = listOf(parent) + ancestors[parent]
-        val children = graph[id].filter { it != root && ancestors[it].isEmpty() }
+        val children = graph[id].filter { parent != it }
         for (child in children) visitVertex(child, id)
         val subtreeData = data[id] + children.map { vertices[it]!!.subtreeSum }.sum()
         vertices[id] = Vertex(ancestors[id], children, subtreeData)
@@ -37,75 +39,70 @@ fun convertGraphToTree(graph: List<List<Int>>, data: List<Int>, root: Int): List
     for (child in graph[root]) visitVertex(child, root)
     val subtreeData = data[root] + graph[root].map { vertices[it]!!.subtreeSum }.sum()
     vertices[root] = Vertex(ancestors[root], graph[root], subtreeData)
-
     return vertices.filterNotNull()
 }
 
+fun <T : Comparable<T>> List<T?>.count(element: T): Int {
+    val i = binarySearch(element)
+    if (i < 0) return 0
+    var j = i
+    var k = i
+    var count = 1
+    while (j-- > 0 && this[j] == element) count++
+    while (k++ < lastIndex && this[k] == element) count++
+    return count
+}
+
 fun balancedForest(graph: List<List<Int>>, c: List<Int>): Long {
+    var minExtra = Long.MAX_VALUE
+
+    fun tryUpdateMinExtra(twinLargerTree: Long, smallerTree: Long) {
+        if (smallerTree <= twinLargerTree) {
+            val candidate = twinLargerTree - smallerTree
+            if (candidate < minExtra) minExtra = candidate
+        }
+    }
+
     val root = findRootOfMinHeightTree(graph).first()
     val vertices = convertGraphToTree(graph, c, root)
-    val sortedAncestors = vertices.map { it.ancestors.sorted() }
-    var extraMin = Long.MAX_VALUE
+    val subtreeSums = vertices.map { it.subtreeSum }.sorted()
     val totalSum = vertices[root].subtreeSum
 
-    fun tryUpdateMinExtra(candidate: Long) {
-        if (candidate < extraMin) extraMin = candidate
-    }
-
-    fun tryUpdateMinExtra(tree1: Long, tree2: Long, tree3: Long) {
-        if (tree1 == tree2 && tree3 <= tree1) {
-            tryUpdateMinExtra(tree1 - tree3)
-        } else if (tree1 == tree3 && tree2 <= tree1) {
-            tryUpdateMinExtra(tree1 - tree2)
-        } else if (tree2 == tree3 && tree1 <= tree2) {
-            tryUpdateMinExtra(tree2 - tree1)
-        }
-    }
-
     for (i in vertices.indices.filter { it != root }) {
-        val subtreeSum = vertices[i].subtreeSum
-        val complementSum = totalSum - subtreeSum
-
-        if (2 * subtreeSum == totalSum) {
-            tryUpdateMinExtra(subtreeSum)
-            continue
-        }
-
-        val (smallerTree, largerTree) = if (2 * subtreeSum < totalSum) {
-            subtreeSum to complementSum
+        val ancestors = vertices[i].ancestors.map { vertices[it].subtreeSum }
+        if (3 * vertices[i].subtreeSum < totalSum) {
+            val smallerTree = vertices[i].subtreeSum
+            if ((totalSum - smallerTree).isEven) {
+                val twinLargerTree = (totalSum - smallerTree) / 2
+                if (ancestors.binarySearch(twinLargerTree + smallerTree) >= 0) {
+                    tryUpdateMinExtra(twinLargerTree, smallerTree)
+                }
+                val count = subtreeSums.count(twinLargerTree)
+                if (count >= 2 || count >= 1 && ancestors.binarySearch(twinLargerTree) < 0) {
+                    tryUpdateMinExtra(twinLargerTree, smallerTree)
+                }
+            }
         } else {
-            complementSum to subtreeSum
-        }
-
-        if (smallerTree * 2 < largerTree && largerTree.isOdd || extraMin < largerTree / 2 - smallerTree) continue
-
-        for (j in i + 1 until vertices.size) {
-            val subtreeSum2 = vertices[j].subtreeSum
-            when {
-                sortedAncestors[i].binarySearch(j) >= 0 ->
-                    tryUpdateMinExtra(
-                        subtreeSum,
-                        subtreeSum2 - subtreeSum,
-                        totalSum - subtreeSum2
-                    )
-                sortedAncestors[j].binarySearch(i) >= 0 ->
-                    tryUpdateMinExtra(
-                        subtreeSum - subtreeSum2,
-                        subtreeSum2,
-                        totalSum - subtreeSum
-                    )
-                else -> tryUpdateMinExtra(
-                    subtreeSum,
-                    subtreeSum2,
-                    totalSum - subtreeSum - subtreeSum2
-                )
+            val twinLargerTree = vertices[i].subtreeSum
+            val smallerTree = totalSum - 2 * twinLargerTree
+            if (ancestors.binarySearch(2 * twinLargerTree) >= 0) {
+                tryUpdateMinExtra(twinLargerTree, smallerTree)
+            }
+            if (ancestors.binarySearch(twinLargerTree + smallerTree) >= 0) {
+                tryUpdateMinExtra(twinLargerTree, smallerTree)
+            }
+            if (subtreeSums.count(twinLargerTree) >= 2) {
+                tryUpdateMinExtra(twinLargerTree, smallerTree)
             }
         }
     }
-    return if (extraMin == Long.MAX_VALUE) -1 else extraMin
+
+    return if (minExtra == Long.MAX_VALUE) -1 else minExtra
 }
 
 fun main() {
+    val stdin = SimulatedStandardInput("input05.txt")
+    fun readLine() = stdin.readLine()
     repeat(readLine().orEmpty().toInt()) {
         val n = readLine().orEmpty().toInt()
         val c = readLine().orEmpty().split(' ').map(String::toInt)
