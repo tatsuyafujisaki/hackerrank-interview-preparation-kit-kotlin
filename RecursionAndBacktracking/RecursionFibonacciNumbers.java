b1e29a9d7fb412f3c70fe64a45e0655259e8be87
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2021-04-26 23:03:28 +0900

    Create RecursionFibonacciNumbers.java

---
+++ RecursionAndBacktracking/RecursionFibonacciNumbers.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static int fibonacci(int n) {
+        if (n <= 1) return n;
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        scanner.close();
+        System.out.println(fibonacci(n));
+    }
+}
