0bb9845130a29d0d0ce87b2c1b11294bac1a07e1
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2021-04-29 22:59:27 +0900

    Update FriendCircleQueries.kt

--- Miscellaneous/FriendCircleQueries.kt
+++ Miscellaneous/FriendCircleQueries.kt
@@ -1,52 +1,27 @@
-class DisjointSets(n: Int) {
-    private val parents = IntArray(n) { it }
-    private val ranks = IntArray(n)
-    private val people = IntArray(n) { 1 }
-    var maxCircle = Int.MIN_VALUE
-
-    /** Using the technique "path compression" */
-    private fun find(x: Int): Int {
-        if (parents[x] == x) return x
-        parents[x] = find(parents[x])
-        return parents[x]
-    }
-
-    /** Using the technique "union by rank" */
-    fun union(x: Int, y: Int) {
-        val xRoot = find(x)
-        val yRoot = find(y)
-        when {
-            ranks[xRoot] < ranks[yRoot] -> {
-                parents[xRoot] = yRoot
-                tryUpdateMaxCircle(xRoot, yRoot)
-            }
-            ranks[xRoot] > ranks[yRoot] -> {
-                parents[yRoot] = xRoot
-                tryUpdateMaxCircle(xRoot, yRoot)
-            }
-            xRoot != yRoot -> {
-                parents[yRoot] = xRoot
-                ranks[xRoot]++
-                tryUpdateMaxCircle(xRoot, yRoot)
-            }
-        }
-    }
-
-    private fun tryUpdateMaxCircle(xRoot: Int, yRoot: Int) {
-        val sum = people[xRoot] + people[yRoot]
-        people[xRoot] = sum
-        people[yRoot] = sum
-        if (maxCircle < sum) maxCircle = sum
-    }
-}
+import kotlin.math.max
 
 fun maxCircle(queries: List<List<Int>>): List<Int> {
     val compress = queries.flatten().toSet().mapIndexed { i, x -> x to i }.toMap()
-    val disjointSets = DisjointSets(compress.size)
+    val parents = IntArray(compress.size) { it }
+    val people = IntArray(compress.size) { 1 }
     val maxCircles = mutableListOf<Int>()
+    var maxCircle = Int.MIN_VALUE
+
+    tailrec fun find(x: Int): Int = if (parents[x] == x) x else find(parents[x])
+
+    fun union(x: Int, y: Int) {
+        val xRoot = find(x)
+        val yRoot = find(y)
+        if (xRoot != yRoot) {
+            parents[yRoot] = xRoot
+            people[xRoot] = people[xRoot] + people[yRoot]
+            maxCircle = max(maxCircle, people[xRoot])
+        }
+    }
+
     for ((a, b) in queries) {
-        disjointSets.union(compress[a]!!, compress[b]!!)
-        maxCircles.add(disjointSets.maxCircle)
+        union(compress[a]!!, compress[b]!!)
+        maxCircles.add(maxCircle)
     }
     return maxCircles
 }
